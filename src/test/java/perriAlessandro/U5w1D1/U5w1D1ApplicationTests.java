d3742671a2330af319f56e75f48f66a8d165fedc
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-04-04 16:43:18 +0200

    problem with file config

--- src/test/java/perriAlessandro/U5w1D1/U5w1D1ApplicationTests.java
+++ src/test/java/perriAlessandro/U5w1D1/U5w1D1ApplicationTests.java
@@ -1,61 +1,29 @@
 package perriAlessandro.U5w1D1;
 
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import perriAlessandro.U5w1D1.entities.Bevande;
+import perriAlessandro.U5w1D1.entities.Pizza;
 
 @SpringBootTest
 class U5w1D1ApplicationTests {
 
+    @Autowired
+    private Pizza margherita;
 
-    @BeforeAll
-    public static void beforeAll() {
-        System.out.println("BEFORE ALL");
-        // Before All è una sorta di "setup iniziale" pre test. Ci consente ad esempio
-        // di riempire un database con dei dati utili per il testing
-    }
+    @Autowired
+    private Bevande acqua;
 
-    @AfterAll
-    public static void afterAll() {
-        System.out.println("AFTER ALL");
-        // After All è utile per ripulire un po' la situazione. Ad esempio ripristinare
-        // un database ad uno stato iniziale dopo che i vari test l'hanno "sporcato"
-    }
 
     @Test
-    public void testSum() {
-        System.out.println("TEST 1");
-        int result = CustomMath.sum(2, 2);
-        int result1 = CustomMath.sum(3, 3);
-
-        assertEquals(4, result);
-        assertEquals(6, result1);
+    public void sum() {
+//        System.out.println("SOMMA TEST");
+//        double uno = margherita.getPrezzo();
+//        double due = acqua.getPrezzo();
+//        double result = CustomMath.sum((int) uno, (int) due);
+//
+//        assertEquals(10, result);
     }
 
-    @Test
-    public void testArraySum() {
-        System.out.println("TEST 2");
-        int result = CustomMath.sum(new int[]{1, 2, 3});
-        assertEquals(6, result);
-    }
-
-    @ParameterizedTest
-    @CsvSource({"1,1,2", "2,2,4", "3,4,7", "5,5,10"})
-    public void testParameterizedSum(int add1, int add2, int expectedResult) {
-        int result = CustomMath.sum(add1, add2);
-        assertEquals(expectedResult, result);
-    }
-
-    @BeforeEach
-    public void beforeEach() {
-        System.out.println("BEFORE EACH");
-    }
-
-    @AfterEach
-    public void afterEach() {
-        System.out.println("AFTER EACH");
-    }
 }
